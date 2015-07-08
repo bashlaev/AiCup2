a24c714099aebe89c0c7fafc52b8e8a362d0d463
bashlaev <bashlaev@gmail.com>
2015-07-08 22:26:59 +0300

    Added GoodSolver

--- AICup15/src/test/java/com/devoler/aicup2/solvers/Solvers.java
+++ AICup15/src/test/java/com/devoler/aicup2/solvers/Solvers.java
@@ -6,8 +6,9 @@ public final class Solvers {
 
 	public static void main(String[] args) {
 		try {
+			new RemoteSolverAdapter("GoodSolver", new GoodSolver(), 8080);
 			new RemoteSolverAdapter("TrivialSolver", new TrivialSolver(), 8084);
-			new RemoteSolverAdapter("TrivialEnhancedSolver", new TrivialEnhancedSolver(), 8080);
+			new RemoteSolverAdapter("TrivialEnhancedSolver", new TrivialEnhancedSolver(), 8085);
 			new RemoteSolverAdapter("InvalidFormatSolver", new InvalidFormatSolver(), 8081);
 			new RemoteSolverAdapter("IllegalMoveSolver", new IllegalMoveSolver(), 8082);
 			new RemoteSolverAdapter("DidNotFinishSolver", new DidNotFinishSolver(), 8083);
